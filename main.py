82171bc6c1476a61c748a3ef5ce2ecc10712af23
lagcom <160274922+lagcom@users.noreply.github.com>
2025-11-04 23:56:49 -0500

    make it so that the reactions and keywords are no longer hard-coded (#2)
    * make it so that the reactions and keywords are no longer hard-coded
    preliminary quick-and-dirty implementation, untested.
    
    * some minor brush-ups
    
    * stop filtering spaces because people say it's annoying

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from ast import Tuple
 import discord
 import asyncio
 import os
+import csv
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,24 +10,40 @@ intents.message_content = True
 bot = discord.Client(intents=intents)
 
 token = os.getenv('NOWEHBOT_TOKEN')
+keywordDict = dict()
 
 if not token:
     with open('token.txt', 'r', encoding='utf8') as tokenFile:
         token = tokenFile.read().strip()
 
+# keyword file reading
+keywordFileString=''
+try:
+    keywordFile = open('keywords.csv', 'r', encoding='utf8')
+except FileNotFoundError:
+    with open('TEMPLATE_keywords.csv','r', encoding='utf8') as keywordFile:
+        keywordFileString=keywordFile.read()
+        with open('keywords.csv','w',encoding='utf8') as newKeywordFile:
+            newKeywordFile.write(keywordFileString)
+    keywordFile = open('keywords.csv', 'r', encoding='utf8')
+
+# make dictionary
+keywordFileDict = csv.DictReader(keywordFile)
+for row in keywordFileDict:
+    keywordDict[row['keyword']]=Tuple(row['emojiName'],row['emojiID'])
+keywordFile.close()
+
 async def handleMessage(message: discord.Message):
     c = []
     
-    filter = '|*_ (){}[];:'
+    filter = '|*_(){}[];:'
     sanitized_content = message.content.lower()
     for char in filter:
         sanitized_content = sanitized_content.replace(char,'')
     
-    if ('weh' in sanitized_content) or ('ɥǝʍ' in sanitized_content):
-        c.append(message.add_reaction('<:noweh:1414871407493517332>'))
-    
-    if 'mizukicry' in sanitized_content:
-        c.append(message.add_reaction('<:mizukicrying:1397846161250979913>'))
+    for keyword,reaction in keywordDict.items():
+        if keyword in sanitized_content:
+            c.append(message.add_reaction(f'<:{reaction[0]}:{reaction[1]}>'))
     
     await asyncio.gather(*c)
 
